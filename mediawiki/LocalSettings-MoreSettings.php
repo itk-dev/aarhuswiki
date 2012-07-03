1abbc99253febb563d53bfed6507ba164245288c
Steen Larsen <sgl@aarhus.dk>
2012-07-03 16:54:57 +0200

    Some settings moved to git

---
+++ mediawiki/LocalSettings-MoreSettings.php
@@ -0,0 +1,13 @@
+<?php
+#
+# This file must NOT contain any sensitiv information
+# 
+#
+
+## CAVENDISH SETTINGS
+$cavendishLogoURL = '';
+$cavendishLogoMargin = '10';
+
+include("$IP/extensions/JavaScript/JavaScript.php");
+include("$IP/extensions/googleAnalytics/googleAnalytics.php" );
+
