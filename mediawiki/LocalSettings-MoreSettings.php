422f29f951ca611a621cc2d9760a7ce3ff1d8987
clausjuhl <clausjuhl@users.noreply.github.com>
2016-07-06 10:13:02 +0200

    Update LocalSettings-MoreSettings.php
    Re-added GoogleAnalytics UA-number

--- mediawiki/LocalSettings-MoreSettings.php
+++ mediawiki/LocalSettings-MoreSettings.php
@@ -9,58 +9,53 @@ if ( !defined( 'MEDIAWIKI' ) ) {
         exit;
 }
 
-
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'standard', 'nostalgia', 'cologneblue', 'monobook', 'vector':
-
 wfLoadSkin( 'CologneBlue' );
 wfLoadSkin( 'Modern' );
 wfLoadSkin( 'MonoBook' );
 wfLoadSkin( 'Vector' );
 
-# New responsive skin
-# require_once( "$IP/skins/filament/filament.php" );
-
-
 $wgDefaultSkin = "cavendishmw";
-require_once("$IP/skins/cavendishmw/cavendishmw.php");
 
+require_once("$IP/skins/cavendishmw/cavendishmw.php");
 ## CAVENDISH SETTINGS
 $cavendishLogoURL = '';
 $cavendishLogoMargin = '10';
 
+# GoogleAnalytics
 include("$IP/extensions/googleAnalytics/googleAnalytics.php" );
+$wgGoogleAnalyticsAccount = 'UA-32768052-1';
+
+# NewestPages
 include("$IP/extensions/NewestPages/NewestPages.php" );
 
-# ConfirmEdit-extension
+# ConfirmEdit
 require_once( "$IP/extensions/ConfirmEdit/ConfirmEdit.php" );
-//require_once("$IP/extensions/ConfirmEdit/QuestyCaptcha.php");
 wfLoadExtension( 'ConfirmEdit/QuestyCaptcha' );
 $wgCaptchaClass = 'QuestyCaptcha';
 $wgCaptchaQuestions[] = array( 'question' => "Indtast postnummeret for Aarhus C i feltet", 'answer' => "8000" );
 
-#User Merge and Delete-extension
+# User Merge and Delete
 //require_once( "$IP/extensions/UserMerge/UserMerge.php" );
 wfLoadExtension( 'UserMerge' );
-
-// By default nobody can use this function, enable for bureaucrat?
+# By default nobody can use this function, enable for bureaucrat
 $wgGroupPermissions['bureaucrat']['usermerge'] = true;
-
-// optional: default is array( 'sysop' )
+# optional: default is array( 'sysop' )
 $wgUserMergeProtectedGroups = array( 'bureaucrat' );
 
-#Renameuser
+# Renameuser
 //require_once("$IP/extensions/Renameuser/Renameuser.php");
 wfLoadExtension( 'Renameuser' );
 
-#WikiEditor - inklusion af nedenstående parametre var også nødvendigt
+# WikiEditor - inklusion af nedenstående parametre var også nødvendigt
 //require_once("$IP/extensions/WikiEditor/WikiEditor.php");
 wfLoadExtension( 'WikiEditor' );
 $wgDefaultUserOptions['usebetatoolbar'] = 1;
 $wgDefaultUserOptions['usebetatoolbar-cgd'] = 1;
 $wgDefaultUserOptions['wikieditor-preview'] = 1;
 
-#Cite
+# Cite
 //require_once("$IP/extensions/Cite/Cite.php");
 wfLoadExtension( 'Cite' );
 
